e48567e18b7a66eeb258a5cedf3ddcc339b443f6
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-09-29 01:18:37 +0500

    Task2

---
+++ Tyuiu.IvanovMS.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.IvanovMS.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.IvanovMS.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessage()
+        {
+            var name = "Максим";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Максим", res);
+        }
+    }
+}
---
+++ Tyuiu.IvanovMS.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿using Tyuiu.IvanovMS.Sprint0.Task2.V0.Lib;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Максим"));
+        Console.ReadKey();
+    }
+}
