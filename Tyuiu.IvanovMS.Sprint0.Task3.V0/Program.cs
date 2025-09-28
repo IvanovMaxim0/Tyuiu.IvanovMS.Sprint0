7ff18c16c7415594cc2fa1a38c28212afa3017fd
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-09-29 01:27:24 +0500

    Task3

---
+++ Tyuiu.IvanovMS.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.IvanovMS.Sprint0.Task3.V0.Lib;
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Sum(7, 7));
+        Console.ReadKey();
+    }
+}
